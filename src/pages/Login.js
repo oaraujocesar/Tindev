22692f42974d0e674034dcbee9d1ba4265f5577a
caraujo07 <caraujo@jc.com.br>
2019-08-07 13:51:12 -0300

    frontend do projeto Tindev da Semana OmniStack 8.0

---
+++ src/pages/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import './Login.css';
+import logo from '../assets/logo.svg';  
+
+import api from '../services/api';
+
+function Login({ history }) {
+
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username,
+    });
+
+    console.log(response);
+
+    history.push('/main');
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={ handleSubmit }>
+        <img src={ logo } alt="Tindev" />
+        <input 
+          placeholder="Digite o seu usuário do GitHub"
+          value={ username }
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  )
+};
+
+export default Login;
